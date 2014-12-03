03c2ba2421f68cec2e4a6ca32627eddf161630a3
amolmorey <amolmorey24@gmail.com>
2014-12-03 17:04:23 +0530

    url path is set for every webpage

--- PMO_website/PMO/urls.py
+++ PMO_website/PMO/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
 	 url(r'^groups/$', views.homepage, name='groups'),
 	 url(r'^(?P<i_id>\d+)/$', views.archive, name='archive'),
 	 url(r'^login/$', views.login_user, name='login_user'),
+	 url(r'^login/a/$', views.logged_user, name='login'),
+	 url(r'^login/submit/$', views.submit, name='login'),
 	url(r'^logout/$', views.logout_user, name='logout_user'),	
 	
 )
