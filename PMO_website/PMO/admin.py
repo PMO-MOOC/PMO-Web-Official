7bdff969c1d43b9746b0606468cf4ec390188302
raehasandalwala <raehasandalwala@gmail.com>
2014-12-17 17:21:31 +0530

    Added model for Forum

--- PMO_website/PMO/admin.py
+++ PMO_website/PMO/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from PMO.models import User
+from PMO.models import Question
+from PMO.models import Post
 
 class UserAdmin(admin.ModelAdmin):
 	fieldsets = [
@@ -10,4 +12,7 @@ class UserAdmin(admin.ModelAdmin):
 		('Announcement', {'fields':['recent_upload'],'classes': ['collapse']})
 		]
 	list_display = ('user_name', 'pub_date')
+        
 admin.site.register(User, UserAdmin)
+admin.site.register(Question)
+admin.site.register(Post)
--- PMO_website/PMO/models.py
+++ PMO_website/PMO/models.py
@@ -14,3 +14,20 @@ class User(models.Model):
 	pub_date = models.DateTimeField('date published', null=True, blank=True)
 	user_name = models.CharField(max_length=200, null=True, blank=True)
 	
+class Question(models.Model):
+	question_id = models.AutoField(max_length=1000,primary_key=True)
+        question = models.TextField(null=False,blank=False)
+	ques_date = models.DateTimeField()
+	userr = models.ForeignKey(User) 
+
+	def __str__(self):             
+            return self.question
+
+class Post(models.Model):
+        ques_id = models.ForeignKey(Question)    
+	userr = models.ForeignKey(User)  
+	post = models.TextField(max_length=500, null=True, blank=True)
+	post_date = models.DateTimeField()
+
+	def __str__(self):             
+            return self.post
